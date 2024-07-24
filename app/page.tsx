8734db69dc9f84a02b13cbb1f8de09f2571ebb1a
nachochiappero <ignaciogabrielchiappero@gmail.com>
2024-07-24 17:00:47 -0300

    implementacion logo y nombre personalizado

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,43 @@ import { ChangeEvent, useState } from "react";
 import Messages from "./component/messages";
 import InputForm from "./component/inputForm";
 import ThemeToggle from "./component/ThemeToggle";
+import Image from "next/image";
 
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit, isLoading, stop } = useChat({
     api: "api/genai",
   });
 
+  const [theme, setTheme] = useState('light');
+
   return (
     <main className="flex flex-col h-screen bg-white dark:bg-gray-900 text-black dark:text-white font-sans overflow-hidden">
-      <div className="p-2  text-2xl bg-gray-300 dark:bg-gray-800 z-10 fixed top-0 left-0 w-full text-center">
+      <div className="p-2 text-2xl bg-gray-300 dark:bg-gray-800 z-10 fixed top-0 left-0 w-full text-center">
         <div className="flex justify-between items-center px-4">
-          <span className="text-black dark:text-white">Nach-AI</span>
-          <ThemeToggle /> {/* Añade el ThemeToggle aquí */}
+
+          {/* Contenedor del título */}
+          <div className="flex items-center ">
+            
+                   
+          <Image
+              src="/LogoO.png"
+              alt="Logo2"
+              width={30}
+              height={30}
+              className="mr-2"
+          />
+            
+            
+
+            <div>
+              <span className="text-black dark:text-white font-orbitron">nIA</span> <span className="text-xs">by</span> <span className="text-sm">Nacho <span className="text-orange-500">Dev</span></span>
+
+            </div>
+
+
+          </div>
+         
+          <ThemeToggle />
         </div>
       </div>
       <div className="flex-grow overflow-auto p-4 pt-20">
